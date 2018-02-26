67bef7d925bf409caa6efcffe7211b31d43ca1fc
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-26 18:23:27 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,9 @@
 def speak_to_grandma(str)
   if str != str.upcase
     "HUH?! SPEAK UP, SONNY!"
-  elsif str = "I LOVE YOU GRANDMA"
+  elsif str == "I LOVE YOU GRANDMA"
     "I LOVE YOU TOO PUMPKIN!"
   else
-    "NO< NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   end
 end
